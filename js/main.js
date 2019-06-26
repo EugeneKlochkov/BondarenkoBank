eb7a97b78a0b5941c520d5e3f2b58464ee94cc01
eugeneklochkov <fop.klochkov@gmail.com>
2019-06-26 13:58:30 +0300

    Added google map

--- js/main.js
+++ js/main.js
@@ -23,10 +23,10 @@
         });
 
         $(window).on('scroll', function () {
-            if ( $(window).scrollTop() > 500 ){
-                $('.top-scroll').css('display','block');
+            if ($(window).scrollTop() > 500) {
+                $('.top-scroll').css('display', 'block');
             } else {
-                $('.top-scroll').css('display','none');
+                $('.top-scroll').css('display', 'none');
             }
         });
 
@@ -37,7 +37,7 @@
 
         $('.nav-footer .nav-title').on('click', function () {
             var $this = $(this);
-            if($this.hasClass('active')) {
+            if ($this.hasClass('active')) {
                 $this.removeClass('active');
             } else {
                 $this
@@ -49,5 +49,40 @@
             }
         });
 
+        $(window).load(function () {
+
+        });
     });
 })(jQuery);
+
+function initMap() {
+    var container = $('#map')[0],
+        content = $('#info-w').html(),
+        coords = {lat: 48.7357985, lng: 37.5847404},
+        image = './img/logo.png',
+        map = new google.maps.Map(container, {
+            center: coords,
+            zoom: 12,
+            disableDefaultUI: true,
+        }),
+        marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            //icon: image,
+            animation: google.maps.Animation.DROP,
+            //animation: google.maps.Animation.BOUNCE,
+            title: 'Bango Bank'
+        }),
+        infowindow = new google.maps.InfoWindow({
+            content: content
+        });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+    /*Центрирование маркера при изменении размера карты*/
+    google.maps.event.addDomListener(window, 'resize', function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+    });
+}
